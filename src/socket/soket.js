a767e8edb26f898d4fc453b3fdbe9e6123c7e969
Om Gupta <guptaomg23@gmail.com>
2026-06-03 01:00:47 +0530

    soket connection done

--- src/socket/soket.js
+++ src/socket/soket.js
@@ -1,8 +1,15 @@
-const { Socket } = require("socket.io");
-
 const socketHandler = (io) => {
-  io.on("connection", (soket) => {
-    console.log("Soket Connection Istablihed");
+  io.on("connection", (socket) => {
+    console.log("Socket Connected");
+
+    socket.on("joinChat", (userId) => {
+      socket.join(userId);
+    });
+
+    socket.on("sendMessage", (data) => {
+      io.to(data.receiverId).emit("receiveMessage", data);
+    });
   });
 };
+
 module.exports = socketHandler;
